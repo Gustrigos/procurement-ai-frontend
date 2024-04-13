bcdc0e8a7dd9d143082e2b890bd67f87ecbaccd8
Gus Trigos <gus@getmentum.com>
2024-04-13 14:56:01 -0700

    second commit

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -7,7 +7,7 @@ function Topbar() {
 
         <Link href='/' className='flex items-center gap-4'>
           <Image src='/assets/logo.png' alt='logo' width={28} height={28} />
-          <p className='text-heading3-bold text-light-2 max-xs:hidden'>Mentum AI</p>
+          <p className='text-heading3-bold text-light-2 max-xs:hidden'>Procurement AI</p>
         </Link>
 
     </nav>
